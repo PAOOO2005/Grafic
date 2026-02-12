ade0144e9910c77ead435cc65bd9466916251c1d
Pao <garciagarciapaola128@gmail.com>
2026-02-12 13:28:47 -0600

    segundo cambio

--- imagem.py
+++ imagem.py
@@ -1,7 +1,12 @@
 import cv2 as cv
 import numpy as np 
 
-img = np.ones((400,400), np.uint8)
+img = np.ones((400,400), np.uint8)*255
+i=j=0
+for i in range(180, 200):
+    for j in range(180, 200):
+        img[i,j]=180
+
 cv.imshow('img', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
