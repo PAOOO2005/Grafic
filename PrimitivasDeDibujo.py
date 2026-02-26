7fff0ece33e2c67342a6d0361d7a373b888d1e92
Pao <garciagarciapaola128@gmail.com>
2026-02-26 10:46:30 -0600

    Oso con patas y mano

--- PrimitivasDeDibujo.py
+++ PrimitivasDeDibujo.py
@@ -4,29 +4,54 @@ import numpy as np
 # Creamos un lienzo de 500x500 con 3 canales (RGB) para tener colores
 img = np.ones((500, 500, 3), np.uint8) * 255 
 
-# --- Partes del Oso ---
+# Partes del Oso
 # Orejas
-cv.circle(img, (180, 150), 60, (0, 0, 0), -1)  # Oreja Izq
-cv.circle(img, (320, 150), 60, (0, 0, 0), -1)  # Oreja Der
+cv.circle(img, (180, 110), 60, (0, 0, 0), -1)  # Oreja Izq
+cv.circle(img, (320, 110), 60, (0, 0, 0), -1)  # Oreja Der
 
 # Cabeza (Círculo principal)
-cv.circle(img, (250, 220), 108, (203, 192, 255), -1) # Color rosa claro
-cv.circle(img, (250, 220), 108, (0, 0, 0), 2)       # Contorno
+cv.circle(img, (250, 170), 108, (203, 192, 255), -1) # Color rosa claro
+cv.circle(img, (250, 170), 108, (0, 0, 0), 2)       # Contorno
 
 # Hocico
-cv.circle(img, (250, 260), 50, (255, 255, 255), -1)
-cv.circle(img, (250, 250), 25, (0, 0, 0), -1)       # Nariz
+cv.circle(img, (250, 230), 40, (255, 255, 255), -1)
+cv.circle(img, (250, 230), 20, (0, 0, 0), -1)       # Nariz
 
 # Ojos
-cv.circle(img, (215, 200), 12, (0, 0, 0), -1)       # Ojo Izq
-cv.circle(img, (285, 200), 12, (0, 0, 0), -1)       # Ojo Der
+cv.circle(img, (215, 180), 15, (0, 0, 0), -1)       # Ojo Izq
+cv.circle(img, (285, 180), 15, (0, 0, 0), -1)       # Ojo Der
 
 # Cuerpo (Rectángulo redondeado o base)
 #cv.rectangle(img, (180, 320), (320, 450), (203, 192, 255), -1)
 #cv.rectangle(img, (180, 320), (320, 450), (0, 0, 0), 2)
 #cuerpo (circulo)
-cv.circle(img, (250, 415), 90, (203, 192, 255), -1)
-cv.circle(img, (250, 415), 90, (0, 0, 0), 2)
+cv.circle(img, (250, 367), 90, (203, 192, 255), -1)
+cv.circle(img, (250, 367), 90, (0, 0, 0), 2)
+
+#Mano izq
+cv.circle(img, (140, 320), 30, (203,192, 255), -1)
+cv.circle(img, (140, 320), 30, (0, 0, 0), 2)
+
+#Mano der
+cv.circle(img, (360, 320), 30, (203,192, 255), -1)
+cv.circle(img, (360, 320), 30, (0, 0, 0), 2)
+
+#Pata Izq
+cv.circle(img, (353, 430), 30, (203,192, 255), -1)
+cv.circle(img, (353, 430), 30, (0, 0, 0), 2)
+
+#Pata Der
+cv.circle(img, (145, 430), 30, (203,192, 255), -1)
+cv.circle(img, (145, 430), 30, (0, 0, 0), 2)
+
+#Cicatriz en la cabeza
+cv.line(img, (180,180), (200,100), (0, 0, 0), 2)
+
+#Hombligo
+cv.circle(img, (250, 430), 10, (0, 0, 0), -1)  
+
+
+
 cv.imshow('Oso', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
