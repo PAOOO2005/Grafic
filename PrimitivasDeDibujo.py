cbf828c9a2704b0b54443040b14de60b2063b40b
Pao <garciagarciapaola128@gmail.com>
2026-02-25 22:22:22 -0600

    oso sin patas y manos

--- PrimitivasDeDibujo.py
+++ PrimitivasDeDibujo.py
@@ -6,25 +6,27 @@ img = np.ones((500, 500, 3), np.uint8) * 255
 
 # --- Partes del Oso ---
 # Orejas
-cv.circle(img, (180, 150), 40, (0, 0, 0), -1)  # Oreja Izq
-cv.circle(img, (320, 150), 40, (0, 0, 0), -1)  # Oreja Der
+cv.circle(img, (180, 150), 60, (0, 0, 0), -1)  # Oreja Izq
+cv.circle(img, (320, 150), 60, (0, 0, 0), -1)  # Oreja Der
 
 # Cabeza (Círculo principal)
-cv.circle(img, (250, 220), 100, (200, 222, 235), -1) # Color crema/claro
-cv.circle(img, (250, 220), 100, (0, 0, 0), 2)       # Contorno
+cv.circle(img, (250, 220), 108, (203, 192, 255), -1) # Color rosa claro
+cv.circle(img, (250, 220), 108, (0, 0, 0), 2)       # Contorno
 
 # Hocico
-cv.circle(img, (250, 260), 35, (255, 255, 255), -1)
-cv.circle(img, (250, 250), 10, (0, 0, 0), -1)       # Nariz
+cv.circle(img, (250, 260), 50, (255, 255, 255), -1)
+cv.circle(img, (250, 250), 25, (0, 0, 0), -1)       # Nariz
 
 # Ojos
 cv.circle(img, (215, 200), 12, (0, 0, 0), -1)       # Ojo Izq
 cv.circle(img, (285, 200), 12, (0, 0, 0), -1)       # Ojo Der
 
 # Cuerpo (Rectángulo redondeado o base)
-cv.rectangle(img, (180, 320), (320, 450), (200, 222, 235), -1)
-cv.rectangle(img, (180, 320), (320, 450), (0, 0, 0), 2)
-
+#cv.rectangle(img, (180, 320), (320, 450), (203, 192, 255), -1)
+#cv.rectangle(img, (180, 320), (320, 450), (0, 0, 0), 2)
+#cuerpo (circulo)
+cv.circle(img, (250, 415), 90, (203, 192, 255), -1)
+cv.circle(img, (250, 415), 90, (0, 0, 0), 2)
 cv.imshow('Oso', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
