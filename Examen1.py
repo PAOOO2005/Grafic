dbf47cd30192e0c3bc9783c8b1f474806e7879aa
Pao <garciagarciapaola128@gmail.com>
2026-03-12 13:53:00 -0600

    Examen1Mision1

---
+++ Examen1.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+# Cargar la imagen en escala de grises
+img = cv2.imread('C:\\Users\\adria\\Desktop\\entorno\\TareasGrafi\\m1_oscura.png', cv2.IMREAD_GRAYSCALE)
+
+# --- MODO RAW ---
+# Obtenemos las dimensiones para el recorrido
+h, w = img.shape
+img_raw = np.zeros((h, w), dtype=np.uint8)
+
+# Recorre con un for y multiplica por 50
+for i in range(h):
+    for j in range(w):
+        img_raw[i, j] = np.clip(int(img[i, j]) * 50, 0, 255)
+
+# --- MODO OPENCV ---
+# Usa la magia de la vectorización 
+img_opencv = cv2.multiply(img, 50)
+
+cv2.imshow('Mensaje Raw', img_raw)
+cv2.imshow('Mensaje OpenCV', img_opencv)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
